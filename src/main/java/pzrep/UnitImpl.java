28654cf848bb541567c45a35a5d8b0c65af175c8
Piotrek Żygieło <pzygielo@users.noreply.github.com>
2022-11-30 17:31:36 +0100

    Reproducer for ClosedFileSystemException

---
+++ src/main/java/pzrep/UnitImpl.java
@@ -0,0 +1,11 @@
+package pzrep;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
+
+public class UnitImpl implements Serializable {
+  void writeObject(OutputStream out) {
+  }
+}
+
